24bcb23945f023abc0cff09620f754112ccce687
kgjhgdgjhk <bhrwrrhsyn@gmail.com>
2025-12-31 19:18:22 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.database import Base, engine
-from app.routers import projects
+from .database import Base, engine
+from .routers import projects
 
 Base.metadata.create_all(bind=engine)
 
